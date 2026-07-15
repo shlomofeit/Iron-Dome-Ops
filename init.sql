67af67d5588d135152cd8bee3a599eff5b085365
Shlomo Feit <11.5sfeit@gmail.com>
2026-07-15 11:51:38 +0300

    Added init.sql with all tables sql code

---
+++ init.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS iron_dome;
+
+USE iron_dome;
+CREATE TABLE IF NOT EXISTS operators (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    soldier_runk VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS incidents (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code_name VARCHAR(100),
+    threat_level VARCHAR(50),
+    status VARCHAR(50),
+    operator_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (operator_id) REFERENCES operators(id)
+);
+
+CREATE TABLE IF NOT EXISTS logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    action VARCHAR(100),
+    incident_id INT,
+    operator_id INT,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (incident_id) REFERENCES incidents(id),
+    FOREIGN KEY (operator_id) REFERENCES operators(id)
+);
