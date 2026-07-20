3c99893c2930578523421e0cb00af2e17989c9e2
shlomofeit <11.5sfeit@gmail.com>
2026-07-20 17:34:29 +0300

    Wire up routes in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { errorHandler, logger } from "./middlewares.js";
+import operatorsRoute from "./routes/operatorsRoute.js";
+import incidentsRoute from "./routes/incidentsRoute.js";
 
 const PORT = process.env.PORT;
 const app = express();
@@ -9,7 +11,8 @@ app.use(express.json());
 app.use(logger);
 
 // routes
-app.use("/operators");
+app.use("/operators", operatorsRoute);
+app.use("/incidents", incidentsRoute);
 
 // error handler
 app.use(errorHandler);
