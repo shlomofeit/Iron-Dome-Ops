e46c063ba5afe80441d59d7bd7e418375e8e198c
shlomofeit <11.5sfeit@gmail.com>
2026-07-20 17:34:22 +0300

    Add operators route

--- src/routes/operatorsRoute.js
+++ src/routes/operatorsRoute.js
@@ -1,1 +1,8 @@
 import express from "express";
+import { createOperator } from "../controllers/operators.js";
+
+const router = express.Router();
+
+router.post("/", createOperator);
+
+export default router;
